f407833170a1c0c712032cbaa9f42b0043264f09
yu-tooo <teg.moritayuto@gmail.com>
2025-08-11 19:23:22 +0900

    set db

--- src/myapp/myapp/settings.py
+++ src/myapp/myapp/settings.py
@@ -74,8 +74,15 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test_app_db',
+        'USER': 'testuser',
+        'PASSWORD': 'test1234',
+        'HOST' : 'db',
+        'PORT' : '3306',
+        'OPTIONS': {
+            'charset' : 'utf8mb4'
+        }
     }
 }
 
